53cf6d385fa1e41ea3f8868b5a15d6444ac01804
azwaradli <azwar.adli@gmail.com>
2017-03-23 20:13:32 +0700

    add main menu page, init lihat katalog, daftar kelompok arisan, menu ketua arisan page

---
+++ FinalProject/app/src/main/java/com/example/azwar/finalproject/KatalogActivity.java
@@ -0,0 +1,17 @@
+package com.example.azwar.finalproject;
+
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class KatalogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_katalog);
+    }
+
+}
--- FinalProject/app/src/main/java/com/example/azwar/finalproject/MainMenuActivity.java
+++ FinalProject/app/src/main/java/com/example/azwar/finalproject/MainMenuActivity.java
@@ -1,7 +1,11 @@
 package com.example.azwar.finalproject;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+//        ActionBar toolbar = getSupportActionBar();
+//        toolbar.setTitle("Arisan Mapan");
+
+    }
+
+    public void lihatKatalog(View view){
+        Intent intent = new Intent(this, KatalogActivity.class);
+        startActivity(intent);
+    }
+
+    public void daftarKelompokArisan(View view){
+        Intent intent = new Intent(this, DaftarKetuaArisanActivity.class);
+        startActivity(intent);
+    }
+
+    public void menuKetuaArisan(View view){
+        Intent intent = new Intent(this, MenuKetuaArisanActivity.class);
+        startActivity(intent);
     }
 }
