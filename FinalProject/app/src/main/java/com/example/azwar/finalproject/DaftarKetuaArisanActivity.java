81f7c0ff19721f88eaf7375bde55e8b16a0e99f7
azwaradli <azwar.adli@gmail.com>
2017-03-24 11:33:59 +0700

    add progress tab, pilih periode, pilih jumlah anggota, and anggota 1 field

--- FinalProject/app/src/main/java/com/example/azwar/finalproject/DaftarKetuaArisanActivity.java
+++ FinalProject/app/src/main/java/com/example/azwar/finalproject/DaftarKetuaArisanActivity.java
@@ -2,6 +2,10 @@ package com.example.azwar.finalproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.lang.reflect.Array;
 
 public class DaftarKetuaArisanActivity extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class DaftarKetuaArisanActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daftar_ketua_arisan);
+
+        Spinner spinner = (Spinner) findViewById(R.id.anggota_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.angota_mingguan_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
