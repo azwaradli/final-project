bc12cc0ce868c56150398453938b0afc56604747
azwaradli <azwar.adli@gmail.com>
2017-04-02 01:47:29 +0700

    add dynamic load on anggota in daftar kelompok arisan page

--- FinalProject/app/src/main/java/com/example/azwar/finalproject/DaftarKelompokArisanActivity.java
+++ FinalProject/app/src/main/java/com/example/azwar/finalproject/DaftarKelompokArisanActivity.java
@@ -1,14 +1,27 @@
 package com.example.azwar.finalproject;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class DaftarKelompokArisanActivity extends AppCompatActivity {
+public class DaftarKelompokArisanActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private Spinner spinner;
 
@@ -18,6 +31,7 @@ public class DaftarKelompokArisanActivity extends AppCompatActivity {
         setContentView(R.layout.activity_daftar_kelompok_arisan);
 
         spinner = (Spinner) findViewById(R.id.anggota_spinner);
+        spinner.setOnItemSelectedListener(this);
 
         RadioButton radioBulanan = (RadioButton) findViewById(R.id.radio_bulanan);
         radioBulanan.setChecked(true);
@@ -50,5 +64,70 @@ public class DaftarKelompokArisanActivity extends AppCompatActivity {
                 }
                 break;
         }
+    }
+
+    public static int convertDPtoPixels(float px, Context context){
+        Resources resources = context.getResources();
+        DisplayMetrics metrics = resources.getDisplayMetrics();
+        float _px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, px, metrics);
+        return (int) _px;
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        String content = parent.getItemAtPosition(pos).toString();
+        int num = Integer.parseInt(content);
+
+        LinearLayout anggotaLayout = (LinearLayout) findViewById(R.id.anggota_section);
+        anggotaLayout.removeAllViews();
+        for(int i = 0; i < num; i++){
+            LinearLayout layout = new LinearLayout(this);
+            layout.setOrientation(LinearLayout.VERTICAL);
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            layoutParams.bottomMargin = convertDPtoPixels(20,this);
+
+            TextView text = new TextView(this);
+            text.setText("Anggota "+(i+1));
+            text.setTextColor(ContextCompat.getColor(this, R.color.primaryText));
+            layout.addView(text, new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+
+            EditText nama = new EditText(this);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            params.topMargin = convertDPtoPixels(8,this);
+            nama.setLayoutParams(params);
+            nama.setPadding(convertDPtoPixels(8, this), convertDPtoPixels(0, this), convertDPtoPixels(0, this), convertDPtoPixels(12, this));
+            nama.setHint("Nama");
+            nama.setHintTextColor(ContextCompat.getColor(this, R.color.hintText));
+            nama.setTextSize(TypedValue.COMPLEX_UNIT_SP,14);
+            layout.addView(nama, params);
+
+            EditText noHP = new EditText(this);
+            noHP.setLayoutParams(params);
+            noHP.setPadding(convertDPtoPixels(8, this), convertDPtoPixels(0, this), convertDPtoPixels(0, this), convertDPtoPixels(12, this));
+            noHP.setHint("No. HP");
+            noHP.setHintTextColor(ContextCompat.getColor(this, R.color.hintText));
+            noHP.setTextSize(TypedValue.COMPLEX_UNIT_SP,14);
+            layout.addView(noHP, params);
+
+            Button button = new Button(this);
+            LinearLayout.LayoutParams buttonParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            buttonParams.topMargin = convertDPtoPixels(8,this);
+            button.setText("pilih barang");
+            button.setBackground(ContextCompat.getDrawable(this, R.drawable.orangeripple));
+            button.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(DaftarKelompokArisanActivity.this, KatalogActivity.class);
+                    startActivity(intent);
+                }
+            });
+            layout.addView(button, buttonParams);
+
+            anggotaLayout.addView(layout,layoutParams);
+        }
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
     }
 }
